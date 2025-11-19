d07a83bf7230361c6c490e27a7699e50180728c7
Luis Rodriguez <luisrdgzalm@gmail.com>
2025-11-19 14:30:06 -0600

    feat: Calculadora con Switch

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function suma (numero1, numero2){
-    return resultado = parseFloat(numero1) + parseFloat(numero2);
+    return parseFloat(numero1) + parseFloat(numero2);
 }
 
 function resta (numero1, numero2){
-    return resultado = parseFloat(numero1) - parseFloat(numero2);
+    return parseFloat(numero1) - parseFloat(numero2);
 }
 
 function multiplicacion (numero1, numero2){
-    return resultado = parseFloat(numero1) * parseFloat(numero2);
+    return parseFloat(numero1) * parseFloat(numero2);
 }
 
 function division (numero1, numero2){
-    return resultado = parseFloat(numero1) / parseFloat(numero2);
+    return parseFloat(numero1) / parseFloat(numero2);
 }
 
 //Prueba
@@ -34,6 +34,34 @@ console.log(division(numero1, numero2));
 
 //Petición de números
 //alert("Bienvenido a la Calculadora en línea");
-//let num1 = prompt("Ingrese el primer número: ");
-//let num2 = prompt("ingrese el segundo número: ");
+let respuesta = "y"
+while(respuesta == "y"){
+
+    let peticion = prompt("¿Qué operación quieres realizar? (1: suma, 2: resta, 3: multiplicación, 4: división)");
+    let num1 = prompt("Ingrese el primer número: ");
+    let num2 = prompt("ingrese el segundo número: ");
+    let resultado = 0;
+    switch(peticion){
+        case "1":
+            console.log(resultado = suma(num1, num2));
+        break; 
+
+        case "2":
+            console.log(resultado = resta(num1, num2));
+        break;
+
+        case "3":
+            console.log(resultado = multiplicacion(num1, num2));
+        break;
+
+        case "4":
+            console.log(resultado = division(num1, num2));
+        break;
+        
+        default: 
+            console.log("Esa operación no se encuentra disponible");
+        break;
+    }
+    respuesta = prompt("¿Quieres realizar una operación? (y/n)");
+}
 
