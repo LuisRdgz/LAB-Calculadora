59c01f704cff684eadb69ec3724e03a1ce806dbe
Luis Rodriguez <luisrdgzalm@gmail.com>
2025-11-19 13:24:00 -0600

    test: pruebas de funciones

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function multiplicacion (numero1, numero2){
 function division (numero1, numero2){
     return resultado = numero1 / numero2;
 }
+
+//prueba
+//console.log(suma(5,5));
+//console.log(resta(5,5));
+//console.log(multiplicacion(5,5));
+//console.log(division(5,5));
