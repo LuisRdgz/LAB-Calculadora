d8c3a70dff506c48c3ec7cd04cf70ea691ae3c21
Luis Rodriguez <luisrdgzalm@gmail.com>
2025-11-19 13:19:45 -0600

    feat: funciones básicas

---
+++ script.js
@@ -0,0 +1,15 @@
+function suma (numero1, numero2){
+    return resultado = numero1 + numero2;
+}
+
+function resta (numero1, numero2){
+    return resultado = numero1 - numero2;
+}
+
+function multiplicacion (numero1, numero2){
+    return resultado = numero1 * numero2;
+}
+
+function division (numero1, numero2){
+    return resultado = numero1 / numero2;
+}
