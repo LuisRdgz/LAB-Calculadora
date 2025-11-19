c1bbf90480ca41bada8191559f2de04f86650ca0
Luis Rodriguez <luisrdgzalm@gmail.com>
2025-11-19 13:31:27 -0600

    feat: Integración de parseFloat

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function suma (numero1, numero2){
-    return resultado = numero1 + numero2;
+    return resultado = parseFloat(numero1) + parseFloat(numero2);
 }
 
 function resta (numero1, numero2){
-    return resultado = numero1 - numero2;
+    return resultado = parseFloat(numero1) - parseFloat(numero2);
 }
 
 function multiplicacion (numero1, numero2){
-    return resultado = numero1 * numero2;
+    return resultado = parseFloat(numero1) * parseFloat(numero2);
 }
 
 function division (numero1, numero2){
-    return resultado = numero1 / numero2;
+    return resultado = parseFloat(numero1) / parseFloat(numero2);
 }
 
 //prueba
@@ -19,3 +19,11 @@ function division (numero1, numero2){
 //console.log(resta(5,5));
 //console.log(multiplicacion(5,5));
 //console.log(division(5,5));
+
+//Simulación
+
+//let numero1 = "10";
+//let numero2 = "20";
+
+//console.log(suma(numero1, numero2));
+//console.log(division(numero1, numero2));
